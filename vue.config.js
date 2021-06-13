60005c3dd61802b0e9756e1a9179dc151481ab2d
tenutes <leopard19670508>
2021-06-13 10:11:46 +0300

    update output dir

--- vue.config.js
+++ vue.config.js
@@ -12,17 +12,7 @@ module.exports = {
     },
   },
 
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(args => {
-        args.compilerOptions.whitespace = 'preserve';
-      });
-    config.plugins.delete('prefetch');
-  },
-
-  outputDir: 'public',
+  outputDir: 'dist',
 
   pwa: {
     themeColor: null,
